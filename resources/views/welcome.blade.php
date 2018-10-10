cca2d233635955e83fbdf8b5474bad39520f304e
dabsin-fullscale <43990597+dabsin-fullscale@users.noreply.github.com>
2018-10-10 19:48:39 +0800

    little change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel by Mike
                 </div>
 
                 <div class="links">
